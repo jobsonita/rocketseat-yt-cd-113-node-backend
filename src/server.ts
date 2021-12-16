4c9bbd410bf5f1c88b3148bb2cc747ecfe908a20
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2021-12-16 18:25:38 -0300

    Configured server routes

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,12 @@ import express from "express";
 
 import "./database";
 
+import { routes } from "./routes";
+
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, () => console.log("Server started on port 3000"));
