4c48e6b7c0475d1b94c235a8001a32db4afcaafb
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2021-12-16 18:42:37 -0300

    Added GET /categories route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { ListAllCategoriesController } from "./controllers/ListAllCategoriesController";
 
 const routes = Router();
 
 routes.post("/categories", new CreateCategoryController().handle);
+routes.get("/categories", new ListAllCategoriesController().handle);
 
 export { routes };
