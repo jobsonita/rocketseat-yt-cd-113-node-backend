86fb184ad02fe967eb03f0331f97f318409e89e2
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2021-12-16 22:17:18 -0300

    Implemented ListAllVideosService

---
+++ src/services/ListAllVideosService.ts
@@ -0,0 +1,13 @@
+import { getRepository } from "typeorm";
+
+import { Video } from "../entities/Video";
+
+export class ListAllVideosService {
+  async execute() {
+    const repo = getRepository(Video);
+
+    const videos = await repo.find();
+
+    return videos;
+  }
+}
