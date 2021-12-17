b193e419ba827b9b1ed5748911b0978b24908235
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2021-12-16 22:23:30 -0300

    Added GET /videos route

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { ListAllCategoriesController } from "./controllers/ListAllCategoriesCont
 import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
 
 import { CreateVideoController } from "./controllers/CreateVideoController";
+import { ListAllVideosController } from "./controllers/ListAllVideosController";
 
 const routes = Router();
 
@@ -15,5 +16,6 @@ routes.delete("/categories/:id", new DeleteCategoryController().handle);
 routes.put("/categories/:id", new UpdateCategoryController().handle);
 
 routes.post("/videos", new CreateVideoController().handle);
+routes.get("/videos", new ListAllVideosController().handle);
 
 export { routes };
