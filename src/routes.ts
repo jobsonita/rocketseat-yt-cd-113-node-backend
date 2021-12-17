77898ea7ff3b34b7bc48ac1df1dbfb79d2d501c7
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2021-12-16 22:09:43 -0300

    Added POST /videos route

--- src/routes.ts
+++ src/routes.ts
@@ -5,11 +5,15 @@ import { DeleteCategoryController } from "./controllers/DeleteCategoryController
 import { ListAllCategoriesController } from "./controllers/ListAllCategoriesController";
 import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
 
+import { CreateVideoController } from "./controllers/CreateVideoController";
+
 const routes = Router();
 
 routes.post("/categories", new CreateCategoryController().handle);
 routes.get("/categories", new ListAllCategoriesController().handle);
 routes.delete("/categories/:id", new DeleteCategoryController().handle);
 routes.put("/categories/:id", new UpdateCategoryController().handle);
+
+routes.post("/videos", new CreateVideoController().handle);
 
 export { routes };
